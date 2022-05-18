52631a1f148143103d91916006f58f8994fa3815
unknown <adzulaj@gmail.com>
2022-05-18 19:23:55 +0300

    var->let

--- js/scripte.js
+++ js/scripte.js
@@ -1,18 +1,18 @@
 $(document).ready( function() {
-var $submenu = $('.submenu');
-var $mainmenu = $('.mainmenu');
-$submenu.hide();
-$submenu.first().delay(400).slideDown(700);
-$submenu.on('click','li', function() {
-$submenu.siblings().find('li').removeClass('chosen');
-$(this).addClass('chosen');
-});
-$mainmenu.on('click', 'li', function() {
-$(this).next('.submenu').slideToggle().siblings('.submenu').slideUp();
-});
-$mainmenu.children('li:last-child').on('click', function() {
-$mainmenu.fadeOut().delay(500).fadeIn();
-});
+  let $submenu = $('.submenu');
+  let $mainmenu = $('.mainmenu');
+  $submenu.hide();
+  $submenu.first().delay(400).slideDown(700);
+  $submenu.on('click','li', function() {
+    $submenu.siblings().find('li').removeClass('chosen');
+    $(this).addClass('chosen');
+  });
+  $mainmenu.on('click', 'li', function() {
+  $(this).next('.submenu').slideToggle().siblings('.submenu').slideUp();
+  });
+  $mainmenu.children('li:last-child').on('click', function() {
+   $mainmenu.fadeOut().delay(500).fadeIn();
+  });
 });
 
 
@@ -38,4 +38,3 @@ const base = document.querySelector('section.base');
   })();
   /*проверка ветви hw-7
 */
-function(){};
